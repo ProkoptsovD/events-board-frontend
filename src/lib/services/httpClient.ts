b4e83e77d45ade5e95697c5c1dbb2c05ca3d1ba5
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-05-17 18:25:16 +0300

    [Feature] add http client

---
+++ src/lib/services/httpClient.ts
@@ -0,0 +1,47 @@
+type BaseOptions = Omit<RequestInit, "method">;
+
+type GETOptions = Omit<BaseOptions, "body">;
+type POSTOptions = BaseOptions;
+
+const API_URL = process.env.SERVER_URL || "http://localhost:3003";
+
+const baseFetch = async <TData = unknown, TError = unknown>(
+  url: string,
+  options: RequestInit
+): Promise<TData | TError> => {
+  try {
+    const apiUrl = API_URL + url;
+    const response = await fetch(apiUrl, options);
+    const data = response.json();
+
+    return data as TData;
+  } catch (error) {
+    console.log(error);
+    return error as TError;
+  }
+};
+
+export const httpClient = {
+  get: async <TData = unknown, TError = APIRequestError>(
+    url: string,
+    options?: GETOptions
+  ): Promise<APIRequestSuccess<TData> | TError> => {
+    const response = await baseFetch<APIRequestSuccess<TData>, TError>(url, {
+      method: "GET",
+      ...options,
+    });
+
+    return response;
+  },
+  post: async <TData = unknown, TError = APIRequestError>(
+    url: string,
+    options?: POSTOptions
+  ): Promise<APIRequestSuccess<TData> | TError> => {
+    const response = await baseFetch<APIRequestSuccess<TData>, TError>(url, {
+      ...options,
+      method: "POST",
+    });
+
+    return response;
+  },
+};
