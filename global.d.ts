26c59642177e30d0de8f3437e9170d7e3af5eca4
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-05-17 18:23:37 +0300

    [Feature] add global types

--- global.d.ts
+++ global.d.ts
@@ -8,3 +8,37 @@ declare module "*.svg?url" {
   const content: any;
   export default content;
 }
+
+declare module "react-calendar/dist/Calendar.css" {
+  const content: any;
+  export default content;
+}
+
+type PropsWithClassName = {
+  className?: string;
+};
+type Option = { label: string; value: any };
+
+type APIRequestError = {
+  message: string;
+  status: number;
+};
+type APIRequestSuccess<TData = unknown> = {
+  data: TData;
+  meta?: { count: number; totalCount: number };
+};
+
+type BaseSearchParams = {
+  perPage: string;
+  page: string;
+  q?: string;
+};
+
+type EventSearchParams = BaseSearchParams & {
+  sortBy: string;
+};
+
+type PageProps<Params, SearchParams = undefined> = {
+  params: Params;
+  searchParams: SearchParams;
+};
